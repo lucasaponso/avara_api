b52b5d7802f32d761f515dce6b2d8130e99038d9
Lucas Aponso <s3896348@student.rmit.edu.au>
2024-07-22 09:59:16 +1000

    add: added README.md

---
+++ routes/example_route.py
@@ -0,0 +1,18 @@
+from fastapi import HTTPException, Depends, status, APIRouter, Request, Query
+
+router = APIRouter(
+    prefix="/example_route"
+)
+
+"""
+Function Name: getData
+
+Return a string with the parameter that was put into the request
+
+Parameters:
+- parameter (str)
+
+"""
+@router.get("/get/data", tags=["example_route"], status_code=status.HTTP_200_OK, description="A simple get request to retrieve all data.")
+async def getData(parameter: str):
+    return f"Getting data: {parameter}"
